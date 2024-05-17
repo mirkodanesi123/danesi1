0bb5229197b2279c19d73186257b6581812f36a5
mirkodanesi123 <mirkoaly1@gmail.com>
2024-05-17 17:04:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
+  ['393201391411', 'Mirkospiderman😍😍🕷🕸💋', true],
 ] //Numero degli owner
 
 //────────────────────────────
