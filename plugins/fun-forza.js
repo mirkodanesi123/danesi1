39bcdb300790d285d291f11a2e8a80cc9e3521b4
mirkodanesi123 <mirkoaly1@gmail.com>
2024-05-23 18:32:36 +0200

    Create fun-forza.js

---
+++ plugins/fun-forza.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, command, text }) => {
+let love = `*💪Calcolatore di forza💪*
+*La forza suprema di ${text} rispetto a te è* *${Math.floor(Math.random() * 100)}%* *di 100%*
+*Allenati secco di merda😎*
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(crush)$/i
+export default handler
